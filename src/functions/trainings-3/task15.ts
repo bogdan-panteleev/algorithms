085294007e3bcf93a32a26cd5e2cad2b36b60642
panteleev bogdan <panteleevbbbogdan@gmail.com>
2023-12-15 16:35:52 +0300

    Refactor linelandiaMovement function and add documentation

--- src/functions/trainings-3/task15.ts
+++ src/functions/trainings-3/task15.ts
@@ -3,9 +3,20 @@ interface City {
   prices: number;
   index: number;
 }
+
+/**
+ * Calculates the movement in Linelandia.
+ * 
+ * @param prices - An array of prices for each city.
+ * @returns An array representing the movement for each city.
+ */
 export function linelandiaMovement(prices: number[]): number[] {
+  if (prices.length === 0) {
+    return [];
+  }
+
   const stack = new Stack<City>();
-  const result: number[] = new Array(prices.length);
+  const result: number[] = Array.from({length: prices.length});
 
   for (let i = 0; i < prices.length; i++) {
     if (stack.size() === 0) {
@@ -19,7 +30,7 @@ export function linelandiaMovement(prices: number[]): number[] {
     }
   }
 
-  while (stack.size() !== 0) {
+  while (stack.size()) {
     result[stack.pop()!.index] = -1;
   }
 
